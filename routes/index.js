81d89543ecfe063a17550a0caf06e109c81a1f3f
OmerFarukGenc <omer.faruk.genc.cs@gmail.com>
2022-01-14 09:17:39 +0300

    router injection without DRY principle

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,13 @@ const express = require("express")
 const router = express.Router();
 
 
+const path = require("path");
+const fs = require("fs");
+fs.readdirSync(__dirname,{withFileTypes: true})
+.filter(dirent => dirent.isDirectory())
+.forEach(
+    file => {console.log("FILE " + file.name); router.use("/"+file.name,require(path.join(__dirname,file.name,"index")));}
+);
 
 
 router.get("/",(req,res) => {
--- server.js
+++ server.js
@@ -1,27 +1,10 @@
 const express = require("express");
 const app = express();
 const PORT = 8000;
-app.get('/', (req, res) => res.send('Express + TypeScript Server'));
-
-const path = require("path");
-const fs = require("fs");
-fs.readdirSync(path.join(__dirname,"routes"),{withFileTypes: true})
-.filter(dirent => dirent.isDirectory())
-.forEach(
-    file => {console.log("FILE " + file.name);}
-);
+const mainRouter = require("./routes/index");
 
 
-const mainRouter = (() => {
-  const router = express.Router();
-
-  f = (d,router) => {
-
-
-  }
-
-}
-)();
+app.use("/",mainRouter)
 
 
 app.listen(PORT, () => {
