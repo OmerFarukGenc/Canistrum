d4ef817d97bb696bbfe2ab86ad3a953ad6710e76
OmerFarukGenc <omer.faruk.genc.cs@gmail.com>
2022-01-26 17:40:58 +0300

    mongodb

--- user-client/src/App.js
+++ user-client/src/App.js
@@ -2,23 +2,49 @@ import LoginScreen from "./LoginScreen";
 import LoggedScreen from "./LoggedScreen";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Col, Container, Row } from "react-bootstrap";
+import React from "react";
 
-function App() {
-  return (
-    <Container >
-      <Row>
-        <Col className="col-4">
-          <Router>
-            <Routes>
-              <Route path="/login" element={<LoginScreen />} />
+const axios = require("axios");
 
-              <Route path="profile" element={<LoggedScreen />} />
-            </Routes>
-          </Router>
-        </Col>
-      </Row>
-    </Container>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.init();
+  }
+
+
+  async init() {
+    /*try {
+      const res = await axios.get("http://localhost:8000/api/profile/whoami", { withCredentials: true });
+      if (res.status = 200) {
+        //this.setState({ username: res.data.username });
+        window.location = "/profile";
+        return;
+        //return <span>res.data.username</span>;
+      }
+    } catch (err) {
+      console.log(err);
+      window.location = "/login";
+    }*/
+  }
+
+  render() {
+    return (
+      <Container >
+        <Row>
+          <Col className="col-4">
+            <Router>
+              <Routes>
+                <Route path="/login" element={<LoginScreen />} />
+
+                <Route path="/profile" element={<LoggedScreen />} />
+              </Routes>
+            </Router>
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
 }
 
 export default App;
